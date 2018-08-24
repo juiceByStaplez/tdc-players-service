4aa0ae0787d52aaf929fc7729435bc79e9a51f57
matthew@sunriseintegration.com <matthew@sunriseintegration.com>
2018-08-24 15:44:31 -0700

    Initial commit

---
+++ routes/players.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+require("dotenv").config();
+const fs = require("fs");
+const sqlite3 = require("sqlite3").verbose();
+const dbPath =
+  process.env.app_env === "local"
+    ? process.env.local_db_path
+    : process.env.dev_db_path;
+
+const db = new sqlite3.Database(dbPath);
+
+/* GET users listing. */
+router.get("/", function(req, res, next) {
+  let results = [];
+  db.serialize(() => {
+    results = db.all("SELECT * FROM data WHERE speed > 90", (err, rows) => {
+      res.json(rows);
+    });
+  });
+});
+
+module.exports = router;
